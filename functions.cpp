9b7f24b33a50eb774a631254e4ad5c79291a7af4
Qr1te <roman.dashkevich3@gmail.com>
2025-09-09 13:02:44 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -28,8 +28,10 @@ void menu(){
 
     char reset = 'y';
     do {
-        int row1, col1; 
-        int row2, col2; 
+        int row1; 
+        int col1; 
+        int row2; 
+        int col2; 
         int choice;
         std::cout<<"Enter size of the first matrix"<<std::endl;
         setNumber(&row1);
